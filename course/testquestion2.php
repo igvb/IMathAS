2504fbddfe22529349fc92460655a2f89583d5c2
igvb <56183816+igvb@users.noreply.github.com>
2020-07-25 02:03:29 -0400

    Update testquestion2.php
    seed->seed number

--- course/testquestion2.php
+++ course/testquestion2.php
@@ -381,7 +381,7 @@ if ($overwriteBody==1) {
 	}
 
 	printf("<p>"._("Question ID:")." %s.  ", Sanitize::encodeStringForDisplay($qsetid));
-	echo '<span class="small subdued">'._('Seed:').' '.Sanitize::onlyInt($seed) . '.</span> ';
+	echo '<span class="small subdued">'._('Seed Number:').' '.Sanitize::onlyInt($seed) . '.</span> ';
   if ($line['ownerid'] == $userid) {
     echo '<a href="moddataset.php?cid='. Sanitize::courseId($sendcid) . '&id=' . Sanitize::onlyInt($qsetid).'" target="_blank">';
     echo _('Edit Question') . '</a>';
